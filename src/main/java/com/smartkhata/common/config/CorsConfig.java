0e77dd1e3cdca662d68dd813d637d7b51ea51c32
Om <kolekaravdhoot231@gmail.com>
2026-02-21 02:16:09 +0530

    changes in cors files

--- src/main/java/com/smartkhata/common/config/CorsConfig.java
+++ src/main/java/com/smartkhata/common/config/CorsConfig.java
@@ -14,22 +14,21 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
 
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowCredentials(true);
+
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://smart-khata-ochre.vercel.app"
         ));
-        config.setAllowedHeaders(List.of(
-                "Authorization",
-                "Content-Type",
-                "Accept"
-        ));
-        config.setAllowedMethods(List.of(
-                "GET", "POST", "PUT", "DELETE", "OPTIONS"
-        ));
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
